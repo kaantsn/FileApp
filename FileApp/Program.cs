e0a9edfc0bfb53c06def70f30b800be51497f611
Kaan Tosun <tr.kaantsn@gmail.com>
2023-04-16 18:48:29 +0300

    Add project files.

---
+++ FileApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+class Notepad
+{
+    static void Main(string[] args)
+    {
+        string filePath = "";
+        string fileContent = "";
+        string input = "";
+
+        Console.WriteLine("Hoşgeldiniz!");
+
+        while (input.ToLower() != "exit")
+        {
+            Console.WriteLine("Dosya yolu girin veya Yeni dosya oluşturmak için \"new\" yazın:");
+
+            input = Console.ReadLine();
+
+            if (input.ToLower() == "new")
+            {
+                Console.WriteLine("Dosya adı girin:");
+                filePath = Console.ReadLine();
+                Console.WriteLine("Dosya içeriğini girin:");
+                fileContent = Console.ReadLine();
+                File.WriteAllText(filePath, fileContent);
+                Console.WriteLine("Dosya kaydedildi!");
+            }
+            else
+            {
+                try
+                {
+                    filePath = input;
+                    fileContent = File.ReadAllText(filePath);
+                    Console.WriteLine(fileContent);
+
+                    Console.WriteLine("Dosyayı kaydetmek istiyor musunuz? (E/H)");
+                    input = Console.ReadLine();
+
+                    if (input.ToLower() == "e")
+                    {
+                        Console.WriteLine("Dosya içeriğini girin:");
+                        fileContent = Console.ReadLine();
+                        File.WriteAllText(filePath, fileContent);
+                        Console.WriteLine("Dosya kaydedildi!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
